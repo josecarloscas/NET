23f2650b6a4de6ed455909415a1a77a7713369e1
Jose Castro <42446919+josecarloscas@users.noreply.github.com>
2022-11-16 09:51:08 -0500

    Add files via upload

---
+++ WebApplication5/Controllers/CategoriaController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication5.Datos;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        private readonly ApplicationDbContext _db; 
+
+        public CategoriaController (ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Categoria> lista = _db.Categoria;
+
+            return View(lista);
+        }
+
+        //Crear Categoria
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Crear(Categoria categoria)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _db.Categoria.Add(categoria);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        //Get Editar
+        public IActionResult Editar(int? Id)
+        {
+            if(Id==null || Id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Categoria.Find(Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Editar(Categoria categoria)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _db.Categoria.Update(categoria);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        //Get Eliminar
+        public IActionResult Eliminar(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Categoria.Find(Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Eliminar(Categoria categoria)
+        {
+
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+            _db.Categoria.Remove(categoria);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ WebApplication5/Controllers/TipoAplicacionController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication5.Datos;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class TipoAplicacionController : Controller
+    {
+        private readonly ApplicationDbContext _db; 
+
+        public TipoAplicacionController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<TipoAplicacion> lista = _db.TipoAplicacion;
+
+            return View(lista);
+        }
+
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Crear(TipoAplicacion tipoAplicacion)
+        {
+            _db.TipoAplicacion.Add(tipoAplicacion);
+            _db.SaveChanges();
+            
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
